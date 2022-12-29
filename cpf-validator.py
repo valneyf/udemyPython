980524815da157dfe95d3c4ff0040a5de58444c2
Valney Filho <valneyf.dev@gmail.com>
2022-12-29 10:17:37 -0300

    Inclusão de loop para iteração do programa

--- cpf-validator.py
+++ cpf-validator.py
@@ -25,27 +25,30 @@ contrário disso:
 O primeiro dígito do CPF é 7
 """
 
-# Cálculo do primeiro dígito
-cpf = input("Digite um cpf para validar (Ex.: 42990544070): ")
+while True:
+    # Cálculo do primeiro dígito
+    cpf = input("Digite um cpf para validar (Ex.: 42990544070): ")
 
-if not cpf.isnumeric():
-    print("Favor inserir somente números.")
-    cpf = False
+    if not cpf.isnumeric():
+        print("Favor inserir somente números.")
+        break
 
-if len(cpf) != 11:
-    print("Favor inserir cpf válido.")
-    cpf = False
+    if len(cpf) != 11:
+        print("Favor inserir cpf válido.")
+        break
 
-if cpf == True:
-    digit_Sum = 0
-    for num in range(9):
-        print(num, '-', cpf[num], '10 - num = ', 10 - num)
-        digit_Sum += int(cpf[num]) * (10 - num)
-        print(digit_Sum)
+    if cpf == True:
+        digit_Sum = 0
+        for num in range(9):
+            print(num, '-', cpf[num], '10 - num = ', 10 - num)
+            digit_Sum += int(cpf[num]) * (10 - num)
+            print(digit_Sum)
 
-    digit_one = (digit_Sum * 10) % 11
+        digit_one = (digit_Sum * 10) % 11
 
-    if digit_one > 9:
-        digit_one = 0
+        if digit_one > 9:
+            digit_one = 0
 
-    print("O primeiro dígito do CPF informado é: ", digit_one)
+        print("O primeiro dígito do CPF informado é: ", digit_one)
+
+print("Obrigado por utilizar nosso validador de CPF")
