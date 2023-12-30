755d9fdf371763f5db7575e119a3a2c076da8775
Valney Filho <valneyf.dev@gmail.com>
2023-12-30 10:34:05 -0300

    Versao 2 mais pythonic

--- agregacao-composicao.py
+++ agregacao-composicao.py
@@ -14,14 +14,24 @@ Exibir o nome do carro, motor e fabricante na tela
 class Car():
     def __init__(self, name):
         self.name = name
-        self.engine = None
-        self.manufacturer = None
+        self._engine = None
+        self._manufacturer = None
 
-    def set_engine(self, motor):
-        self.engine = motor.name
+    @property
+    def engine(self):
+        return self._engine
+    
+    @engine.setter
+    def engine(self, motor):
+        self._engine = motor.name
 
-    def set_manufacturer(self, vendor):
-        self.manufacturer = vendor.name
+    @property
+    def manufacturer(self):
+        return self._manufacturer
+
+    @manufacturer.setter
+    def manufacturer(self, vendor):
+        self._manufacturer = vendor.name
 
     def __str__(self):
         return f'Carro: {self.name}, Motor: {self.engine}, Fabricante: {self.manufacturer}'
@@ -34,12 +44,20 @@ class Manufacturer():
     def __init__(self, name):
         self.name = name
 
-car1 = Car('Palio')
-
-engine1 = Engine('CVT 1.4')
-car1.set_engine(engine1)
-
+'###############################'
+palio = Car('Palio')
+engine1 = Engine('Fire 1.4')
+palio.engine = engine1
 manufacturer1 = Manufacturer('Fiat')
-car1.set_manufacturer(manufacturer1)
+palio.manufacturer = manufacturer1
 
-print(car1)
+print(palio)
+
+uno = Car('Uno')
+# engine1 = Engine('Fire 1.4')
+uno.engine = engine1
+# manufacturer1 = Manufacturer('Fiat')
+uno.manufacturer = manufacturer1
+
+print(uno)
+
