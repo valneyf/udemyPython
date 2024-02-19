35ced662572c5271d30107baf394c8b060cbf34d
Valney Filho <valneyf.dev@gmail.com>
2024-02-18 22:30:09 -0300

    refactor:Add tipagens e authentication refatorado

--- abs-her-enc-poli.py
+++ abs-her-enc-poli.py
@@ -38,154 +38,174 @@ Banco autentica por um método
 
 from abc import ABC, abstractmethod
 
+
 class Account(ABC):
     def __init__(self, agency: str, account_number: str, balance: float = 0):
-        self._agency = agency
-        self._account_number = account_number
-        self._balance = balance
+        self.agency = agency
+        self.account_number = account_number
+        self.balance = balance
         self._valid = False
 
     @abstractmethod
-    def withdraw(self, value):...
-    
+    def withdraw(self, value): ...
+
     @property
     def valid(self):
         return self._valid
-    
-    @valid.setter
-    def valid(self, value): 
-        self._valid = value        
 
-    def deposit(self, value): 
-        self._balance += value
+    @valid.setter
+    def valid(self, value):
+        self._valid = value
+
+    def deposit(self, value):
+        self.balance += value
         self.details(f'DEPÓSITO R$ {value:.2f}')
 
     def details(self, msg=''):
-        print(f'Seu saldo é: R$ {self._balance:.2f} ({msg})')
+        print(f'Seu saldo é: R$ {self.balance:.2f} ({msg})')
 
-    def __str__(self) -> str:
-        return f'Agencia: {self._agency}, Num. Conta: {self._account_number}, Saldo: R$ {self._balance:.2f}'
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f'({self.agency!r}, {self.account_number!r}, '\
+            f'R$ {self.balance:.2f})'
+        return f'{class_name}{attrs}'
+
 
 class CheckingAccount(Account):
-    def __init__(self, agency: str, account_number: str, balance: float = 0, additional_limit: float = 0):
+    def __init__(self, agency: str, account_number: str, balance: float = 0,
+                 additional_limit: float = 0):
         super().__init__(agency, account_number, balance)
         self._additional_limit = additional_limit
 
     @property
     def additional_limit(self):
         return self._additional_limit
-    
+
     @additional_limit.setter
     def additional_limit(self, additional_limit):
         self._additional_limit = additional_limit
-    
+
     def withdraw(self, value):
         if self.valid:
-            if self._balance + self.additional_limit > 0 and (self._balance + self.additional_limit) - value >= 0:
-                self._balance -= value
+            if self.balance + self.additional_limit > 0 and \
+                    (self.balance + self.additional_limit) - value >= 0:
+                self.balance -= value
                 self.details(f'SAQUE R$ {value:.2f}')
-                return self._balance
+                return self.balance
             else:
-                print(f'Saldo insuficiente')
+                print('Saldo insuficiente')
                 self.details(f'SAQUE NEGADO R$ {value:.2f}')
         else:
-            print('Operação não permitida. Favor efetuar autenticação')    
+            print('Operação não permitida. Favor efetuar autenticação')
 
-    def __str__(self) -> str:
-        return f'{super().__str__()}, Limite Extra: R$ {self.additional_limit:.2f}'
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f'({self.agency!r}, {self.account_number!r}, '\
+            f'R$ {self.balance:.2f}, R$ {self.additional_limit:.2f})'
+        return f'{class_name}{attrs}'
+
 
 class SavingAccount(Account):
     def withdraw(self, value):
         if self.valid:
-            if self._balance > 0 and self._balance - value >= 0:
-                self._balance -= value
+            if self.balance > 0 and self.balance - value >= 0:
+                self.balance -= value
                 self.details(f'SAQUE R$ {value:.2f}')
-                return self._balance
+                return self.balance
             else:
-                print(f'Saldo insuficiente')
+                print('Saldo insuficiente')
                 self.details(f'SAQUE NEGADO R$ {value:.2f}')
         else:
-            print('Operação não permitida. Favor efetuar autenticação')   
+            print('Operação não permitida. Favor efetuar autenticação')
 
-class Person(ABC):
-    def __init__(self, name, age) -> None:
+
+class Person:
+    def __init__(self, name: str, age: int) -> None:
         self._name = name
         self._age = age
-    
+
     @property
-    def name(self): 
+    def name(self):
         return self._name
 
     @name.setter
-    def name(self, name): 
+    def name(self, name):
         self._name = name
 
     @property
-    def age(self): 
+    def age(self):
         return self._age
 
     @age.setter
-    def age(self, age): 
+    def age(self, age):
         self._age = age
 
-    def __str__(self):
-        return f'{self.name}, {self.age}'
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f'({self.name!r}, {self.age!r})'
+        return f'{class_name}{attrs}'
+
 
 class Client(Person):
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int):
         super().__init__(name, age)
-        self._account = ''
+        self.account: Account | None = None
 
-    @property
-    def account(self):
-        return self._account
-    
-    @account.setter
-    def account(self, account):
-        self._account = account
-
-    def __str__(self):
-        return f'Cliente: {self.name}, {self.age}, Conta: {self.account}'
 
 class Bank:
-    def __init__(self):
-        self._clients = []
-        self._accounts = []
-    
-    def insert_clients(self, *clients):
-        self._clients.extend(clients)
-        
-    def insert_accounts(self, *accounts):
-        self._accounts.extend(accounts)
+    def __init__(
+            self,
+            agencies: list[str] | None = None,
+            clients: list[Person] | None = None,
+            accounts: list[Account] | None = None):
+        self.agencies = agencies or []
+        self.clients = clients or []
+        self.accounts = accounts or []
 
-    def authentication(self, client, agency, account_number):
-        c_authenticated = False
-        a_authenticated = False
-        account = None
-        
-        for c in self._clients:
-            if c.name == client:
-                c_authenticated = True
-            
-        for a in self._accounts:
-            if a._agency == agency and a._account_number == account_number:
-                account = a
-                a_authenticated = True
-        
-        if c_authenticated and a_authenticated:
-            print('Cliente autenticado.')
+    def agency_checker(self, account):
+        if account.agency in self.agencies:
+            return True
+        return False
+
+    def client_checker(self, client):
+        if client in self.clients:
+            return True
+        return False
+
+    def account_checker(self, account):
+        if account in self.accounts:
+            return True
+        return False
+
+    def account_client_checker(self, client, account):
+        if account is client.account:
+            return True
+        return False
+
+    def authentication(self, client: Person, account: Account):
+        if self.agency_checker(account) and \
+                self.client_checker(client) and \
+                self.account_checker(account) and \
+                self.account_client_checker(client, account):
             account.valid = True
+            print('Cliente autenticado.')
+
             return True
         else:
-            print('Cliente, Agencia ou Conta informadas não pertencem a este banco')
+            print('Cliente, Agencia ou Conta não pertencem a este banco')
             return False
 
-# checking_account = CheckingAccount('0123', 'x-0123', 120, 100)
+    def __repr__(self):
+        class_name = type(self).__name__
+        attrs = f'({self.agencies!r}, {self.clients!r}, {self.accounts!r})'
+        return f'{class_name}{attrs}'
+
+
 checking_account = CheckingAccount('0123', 'x-0123')
-print(checking_account)
+# print(checking_account)
 
 saving_account = SavingAccount('0456', 'x-0456', 200)
-print(saving_account)
+# print(saving_account)
 
 valney = Client('Valney', 35)
 valney.account = checking_account
@@ -194,14 +214,21 @@ bruna = Client('Bruna', 34)
 bruna.account = saving_account
 print(bruna)
 
-banco1 = Bank()
-banco1.insert_clients(valney, bruna)
-banco1.insert_accounts(checking_account, saving_account)
-banco1.authentication('Valney', '0123', 'x-0123')
-banco1.authentication('Bruna', '0456', 'x-0456')
+banco = Bank()
+print(banco)
+
+banco.clients.extend([valney, bruna])
+banco.accounts.extend([checking_account, saving_account])
+banco.agencies.extend(['0123', '0456'])
+
+print(banco)
+# banco.insert_clients(valney, bruna)
+# banco.insert_accounts(checking_account, saving_account)
+banco.authentication(valney, checking_account)
+# banco.authentication('Bruna', '0456', 'x-0456')
 
 valney.account.withdraw(100)
-valney.account.withdraw(100)
+valney.account.deposit(100)
 valney.account.withdraw(100)
 bruna.account.withdraw(100)
 bruna.account.deposit(100)
